9e0f93d749b1f8be0540ae154b92bebf837192aa
nakamura196 <na.kamura.1263@gmail.com>
2021-05-10 16:35:28 +0900

    fix: bug

--- nuxt.config.js
+++ nuxt.config.js
@@ -286,7 +286,9 @@ export default {
   content: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    standalone: true, //これを追加！
+  },
 
   ...routerBase,
 
