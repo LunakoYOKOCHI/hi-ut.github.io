07e5619cdce02ece61b6c800b44a650b946d7ebf
nakamura196 <nakamura196@users.noreply.github.com>
2021-05-12 03:37:28 +0000

    deploy: e852867aa92978b61ed2871e0350e13c0b425a58

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,6 +1,6 @@
 const process = {
   env: {
     BASE_URL: "https://www.hi.u-tokyo.ac.jp/dev",
-    DATA_URL: "https://hi-ut.github.io",
+    DATA_URL: "https://raw.githubusercontent.com/hi-ut/hi-ut.github.io/master/static", //"https://hi-ut.github.io",
   },
 }
