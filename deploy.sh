9045c5a30ee6d2e8d62a196fa5ee9e9badc0c2c0
nakamura196 <na.kamura.1263@gmail.com>
2021-05-21 16:53:28 +0900

    feat: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
-npm run generate:hi
-\cp -rp dist/* /var/www/html/dev/
-
+rm -rf /home/cert/git/hi-ut.github.io/
+cd /home/cert/git/
+git clone https://github.com/hi-ut/hi-ut.github.io.git
+cd /home/cert/git/hi-ut.github.io/
+npm install
+npm run generate:production
+cp -rp /home/cert/git/hi-ut.github.io/dist/* /web/httpd/siryo/dev/
+rm -rf /home/cert/git/hi-ut.github.io/
+cd /home/cert/
